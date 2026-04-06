13b3c471a176c565ef50e0fdc999abe2689bc1a9
Mujahid <md0n211906@gmail.com>
2026-04-06 15:22:42 +0530

    Update SearchInput.tsx

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,27 +5,29 @@ interface Props {
 }
 
 const SearchInput = ({ onSearch }: Props) => {
-  // Using the Ref hook to access the input value without re-rendering every keystroke
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form style={{ width: '100%', margin: '0 20px' }} onSubmit={(event) => {
-      event.preventDefault();
-      if (ref.current) onSearch(ref.current.value);
-    }}>
+    <form 
+      style={{ width: '100%', margin: '0 20px' }} 
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
       <input 
-  ref={ref}
-  type="text" 
-  placeholder="Search games..." 
-  style={{ 
-    width: '100%', 
-    borderRadius: '20px', 
-    padding: '10px 20px', 
-    border: '1px solid var(--border)', 
-    background: var(--bg-sidebar),     
-    color: 'var(--text-main)'          
-  }}
-/>
+        ref={ref}
+        type="text" 
+        placeholder="Search games..." 
+        style={{ 
+          width: '100%', 
+          borderRadius: '20px', 
+          padding: '10px 20px', 
+          border: '1px solid var(--border)', 
+          background: 'var(--bg-sidebar)', 
+          color: 'var(--text-main)' 
+        }}
+      />
     </form>
   );
 };
